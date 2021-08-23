7b754d5529a1fee8315220e633e532cce9de338b
Ysabel Garcia <ysabel.garcia@live.com>
2021-08-23 22:09:10 +0000

    refactored: w02d1

--- eqArray.js
+++ eqArray.js
@@ -26,9 +26,4 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-eqArrays([1, 2, 3], [1, 2, 3]) // => true
-eqArrays([1, 2, 3], [3, 2, 1]) // => false
-eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
-eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
-
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+module.exports = eqArrays;
---
+++ test/eqArraysTest.js
@@ -0,0 +1,12 @@
+const assertEqual = require("../assertEqual");
+const eqArrays = require("../eqArray");
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
+
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
+
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
+
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);// => false
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
