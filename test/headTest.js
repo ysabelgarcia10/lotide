601b720f68be3e57871c777d65c69e2cc691588c
Ysabel Garcia <ysabel.garcia@live.com>
2021-08-23 23:31:01 +0000

    refactored using Mocha and Chai: w02d1

--- test/headTest.js
+++ test/headTest.js
@@ -1,5 +1,16 @@
-const assertEqual = require("../assertEqual");
-const head = require("../head")
+const assert = require('chai').assert;
+const head   = require('../head');
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5'); 
+  });
+
+  it("returns '3' for [0, 1, 2]", () => {
+    assert.notEqual(head([0, 1, 2]), '3'); 
+  });
+});
