5e6f56a8030d98a629fb6a00e3ab72a812e17ebc
Ysabel Garcia <ysabel.garcia@live.com>
2021-08-24 00:36:50 +0000

    refactored using Mocha and Chai: w02d1

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,10 +1,29 @@
-const assertArraysEqual = require("../assertArraysEqual");
+const assert = require('chai').assert;
 const middle = require("../middle");
 
+describe("middle", () => {
+  it("returns [] for [1, 2]", () => {
+    assert.deepEqual(middle([1]), []);
+  });
 
-assertArraysEqual(middle([1]), []) // => []
-assertArraysEqual(middle([1, 2]), []) // => []
-assertArraysEqual(middle([1, 2, 3]), [2]) // => [2]
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]) // => [3]
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3, 4, 5, 6, 7, 8]) // => [2, 3]
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]) // => [3, 4]
+  it("returns [] for [1]", () => {
+    assert.deepEqual(middle([1]), []); 
+  });
+
+  it("returns [2] for [1, 2, 3]", () => {
+    assert.deepEqual(middle([1, 2, 3]), [2]);
+  });
+
+  it("returns [3] for [1, 2, 3, 4, 5]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5]), [3]); 
+  });
+  
+  it("returns [2, 3, 4, 5, 6, 7, 8] for [1, 2, 3, 4]", () => {
+    assert.notDeepEqual(middle([1, 2, 3, 4]), [2, 3, 4, 5, 6, 7, 8]); 
+  });
+
+  it("returns [2, 3, 4, 5, 6, 7, 8] for [1, 2, 3, 4]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]); 
+  });
+
+});
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,11 +1,13 @@
-const assertEqual = require("../assertEqual");
+const assert = require('chai').assert;
 const tail = require("../tail")
 
+describe("tail", () => {
+  it("returns [2, 3] for [1, 2, 3]", () => {
+    assert.deepEqual(tail([1, 2, 3]), [2, 3]);
+  });
 
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result, ["Lighthouse", "Labs"]); // => will always fail!
+  it("returns ['Lighthouse', 'Labs'] for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.deepEqual(tail(["Hello", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]); 
+  });
 
-//test case
-const words = ["Yo Yo", "Lighthouse", "Labs"]; 
-tail(words);
-assertEqual(words.length, 3);
+});
